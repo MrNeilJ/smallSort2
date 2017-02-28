c6aed6f7e20455103a35189e5556b52204e64895
Neil Johnson <mr.neil.j@gmail.com>
2017-02-27 16:43:05 -0800

    Applied fully fleshed out function for the small sort application.  Utilized pointers to allow for us to modify externally defined variables.

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 
 // PROTOTYPES
-void smallSort2(&a, &b, &c);
+void smallSort2(int &a, int &b, int &c);
 
 int main()
 {
@@ -30,7 +30,30 @@ int main()
     cout << a << ", " << b << ", " << c << endl;
 }
 
-void smallSort2(&a, &b, &c)
+void smallSort2(int &a, int &b, int &c)  // Must call by reference rather than by value
 {
+    // Holding container for moving values
+    int temp;
 
+    // Check to see if A > B, if so flip the values so the smallest is furthest left
+    if (a > b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    // Then compare if the updated A value is greater than C, if so switch values so smallest is left
+    if (a > c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    // Compare the remaining two values to see which is larger, flip acording so smallest number is left
+    if (b > c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
 }
